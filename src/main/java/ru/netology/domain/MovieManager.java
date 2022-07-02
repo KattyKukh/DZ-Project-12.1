8bef289791cb5786883eb3d8201f814a5ce19561
Кухоцкая Екатерина <ekukhotskaya@mail.ru>
2022-07-02 17:04:27 +0300

    Reformat code

--- src/main/java/ru/netology/domain/MovieManager.java
+++ src/main/java/ru/netology/domain/MovieManager.java
@@ -3,7 +3,7 @@ package ru.netology.domain;
 import ru.netology.repository.Repository;
 
 public class MovieManager {
-    private Repository repo;
+    private final Repository repo;
     private int limit = 10;
 
     public MovieManager(Repository repo) {
